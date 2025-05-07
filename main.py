27eb52a14a12099504b26ee7c0985a3520f4cd75
Logeshwari Ramesh <165378801+Logeshwari0809@users.noreply.github.com>
2025-05-07 18:50:50 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,68 @@
+from fastapi import FastAPI, HTTPException
+from rag import get_suggestions,get_summary,general_qna
+import uvicorn
+import json
+from pydantic import BaseModel
+
+from typing import Optional
+from llama_index.vector_stores.elasticsearch import ElasticsearchStore
+from llama_index.core import Document, Settings, VectorStoreIndex, QueryBundle, StorageContext, load_index_from_storage
+from llama_index.llms.gemini import Gemini
+from llama_index.embeddings.gemini import GeminiEmbedding
+
+import os
+API_KEY = os.environ['GEMINI_API_KEY']
+embed_model = GeminiEmbedding(model_name='models/text-embedding-004', api_key=API_KEY)
+Settings.embed_model = embed_model
+
+llm = Gemini(model='models/gemini-1.5-flash', api_key=API_KEY)
+storage_context = StorageContext.from_defaults(persist_dir="notebooks/storage")
+index = load_index_from_storage(storage_context=storage_context, index_id="vector_index")
+query_engine = index.as_query_engine(llm, similarity_top_k=10)
+
+app = FastAPI()
+
+class QueryRequest(BaseModel):
+    query: str
+    restaurant: Optional[str] = None
+
+class QueryResponse(BaseModel):
+    response: str
+
+@app.get('/')
+def home():
+    return {'message': 'connected'}
+
+@app.get("/suggest")
+async def suggest_restaurant(query: str):
+    result = get_suggestions(query)
+    res = result.split('```json')[1].split('```')[0]
+    result = json.loads(res)
+    return result
+
+@app.get("/summary/{restaurant_name}")
+async def summarize(restaurant_name: str):
+    res = get_summary(restaurant_name)
+    res = res.split('```json')[1].split('```')[0]
+    res = json.loads(res)
+    return res
+
+@app.post("/query")
+async def query_index(request: QueryRequest):
+    try:
+        if request.restaurant:
+            user_query = request.query
+            bundle = QueryBundle(query_str=user_query, embedding=Settings.embed_model.get_query_embedding(query=user_query))
+            response = query_engine.query(bundle)
+            return response
+        else:
+            user_query = request.query
+            res = general_qna(user_query)
+            res = res.split('```json')[1].split('```')[0]
+            res = json.loads(res)
+            return {"response":res["answer"]}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Error processing query: {str(e)}")
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
---
+++ rag.py
@@ -0,0 +1,69 @@
+from elasticsearch import Elasticsearch
+es = Elasticsearch('http://localhost:9200/')
+import google.generativeai as genai
+import os
+API_KEY = os.environ['GEMINI_API_KEY']
+genai.configure(api_key=API_KEY )
+
+def create_embedding(text):
+    result = genai.embed_content(model="models/text-embedding-004", content=text)
+    return result['embedding']
+
+def get_context(query):
+    vector = create_embedding(query)
+    knn = {
+        'field': "review_vector",
+        'query_vector': vector,
+        'k':50
+    }
+    res = es.search(index='review_index', knn=knn, source=['restaurant_name','review'])
+    documents = []
+    for doc in res['hits']['hits']:
+        documents.append(str(doc['_source']))
+    return documents
+
+def get_suggestions(query):
+    model = genai.GenerativeModel("gemini-1.5-flash")
+    documents = get_context(query)
+    output_format = "json format(recommendations: {restaurant_name, review, note}. conclusion). the output should contain top recommendations with review text and note (only if there is any negative review). at last a conclusion. note: donot recommend a restaurant if there is negative opinion"
+    prompt = f"You are a resturant suggestor. suggest resturants based on the given data'{query}'.\n\data:\n{''.join(documents)}. output format: {output_format}. donot repeat same restaurant in the output"
+    response = model.generate_content(prompt)
+    return response.text
+
+def get_res_reviews(name):
+    query = {
+        "size":1000,
+    "query":{
+        "match":{
+            "restaurant_name":name
+            }
+        }
+    }
+    res = es.search(index="review_index", body=query,source=['restaurant_name','review'])
+    documents = []
+    for doc in res['hits']['hits']:
+        documents.append(str(doc['_source']))
+    return documents
+
+def get_summary(name):
+    model = genai.GenerativeModel("gemini-1.5-flash")
+    documents = get_res_reviews(name)
+    output_format = "output in json format(restaurant_name, must_try_dishes, highlights, things to note, conclusion, rating). the output should contain best dishes to try(if have data). highlights(if data available). points to keep in mind(if have negative data). at last overall conclusion and overall rating"
+    prompt = f"You are a resturant suggestor. you provide summary of the given restaurant:'{name}' based on given review_index.\n\review_index:\n{''.join(documents)}. output format{output_format}"
+    response = model.generate_content(prompt)
+    return response.text
+
+def restaurant_qna(name, query):
+    model = genai.GenerativeModel("gemini-1.5-flash")
+    documents = get_res_reviews(name)
+    output_format = "json contains restaurant name and answer."
+    prompt = f"You are a resturant suggestor.Answer the user's query:'{query} on a restaurant':'{name}' based on given review_index.\n\review_index:\n{''.join(documents)}. output format: {output_format}"
+    response = model.generate_content(prompt)
+    return response.text
+
+def general_qna(query):
+    model = genai.GenerativeModel("gemini-1.5-flash")
+    output_format = "json contains answer."
+    prompt = f"Answer the user's general query:'{query} output in the provided format. output format: {output_format}"
+    response = model.generate_content(prompt)
+    return response.text
